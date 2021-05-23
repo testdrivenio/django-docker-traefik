84f700d6097cc203445fff5817dc0c9f613d470b
Michael Herman <hermanmu@gmail.com>
2021-05-23 10:21:45 -0500

    refactor

--- users/management/commands/create_user.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from typing import Any, Optional
-
-
-class Command(BaseCommand):
-    def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        User.objects.get_or_create(email="test@test.com")
-        self.stdout.write(self.style.SUCCESS("User added"))
